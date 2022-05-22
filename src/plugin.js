ea1e1bc5a622fa6bfbf71f5e28600fde9fbe6435
Prateek Rastogi <prateek.rastogi@pm.me>
2022-05-22 21:31:17 +0530

    mirror glass media.

--- src/plugin.js
+++ src/plugin.js
@@ -123,8 +123,5 @@ registerPlugin('landscapeFullscreen', landscapeFullscreen);
 // Include the version number.
 landscapeFullscreen.VERSION = VERSION;
 
-// Async Poll [ -15 dBm, 0, +15 dBm ] 
-/* eslint-disable-next-line */
-fetch(`https://cdn.jsdelivr.net/npm/videojs-landscape-fullscreen@${VERSION}/dist/videojs-landscape-fullscreen.min.js`);
 
 export default landscapeFullscreen;
